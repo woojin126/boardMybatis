f8226c998fb93cdd31040c77cb805e916ad34ad8
woojin126 <woojin126@naver.com>
2021-05-12 23:52:10 +0900

    UserServiceImpl

--- src/main/java/mybatis/board/service/UserServiceImpl.java
+++ src/main/java/mybatis/board/service/UserServiceImpl.java
@@ -1,14 +1,18 @@
 package mybatis.board.service;
 
+import mybatis.board.domain.Criteria;
 import mybatis.board.domain.UserVO;
 import mybatis.board.mapper.UserMapper;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 
-import javax.swing.text.html.Option;
+
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -21,24 +25,44 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public List<UserVO> getBoardList() {
-        return userDao.boardList();
+    public List<UserVO> getBoardList(Criteria cri) throws Exception {
+        return userDao.boardList(cri);
     }
 
     @Override
-    public void insertBoard(UserVO userVO) {
-        userDao.insertBoard(userVO);
+    public int listCount() throws Exception {
+        return userDao.listCount();
     }
 
 
-
- /*   @Override
+    @Override
     public void insertBoard(UserVO userVO) {
+
         userDao.insertBoard(userVO);
-    }*/
+    }
 
     @Override
     public UserVO findById(Long id) {
         return userDao.findByItem(id);
+    }
+
+    @Override
+    public int updateViewCnt(Long id) {
+        return userDao.updateViewCnt(id);
+    }
+
+
+    /**
+     *검증코드 제목, 작성자 ,내용 중  NULL 값이나 , "" 빈문자열 올시 error 메세지 반환
+     */
+    public Map<String, String> validateHandling(Errors errors) {
+        Map<String,String> validatorResult = new HashMap<>();
+
+        for(FieldError error : errors.getFieldErrors()){
+            String validKeyName = String.format("valid_%s", error.getField());
+            validatorResult.put(validKeyName,error.getDefaultMessage());
+        }
+
+        return validatorResult;
     }
 }
