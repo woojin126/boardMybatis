8c46de513d85b266b9644722c7508e74ed32fe67
woojin126 <woojin126@naver.com>
2021-05-12 23:50:59 +0900

    UserVO 유효성검사 어노테이션 추가

--- src/main/java/mybatis/board/domain/UserVO.java
+++ src/main/java/mybatis/board/domain/UserVO.java
@@ -1,17 +1,28 @@
 package mybatis.board.domain;
 import lombok.Getter;
 import lombok.Setter;
-import org.jetbrains.annotations.NotNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 
+/**
+ * @valid 유효성 검증을위해 @NotEmpty @NotBlank 설정 
+ * 공백문자나 , NULL 오면 Errors에 들어감
+ */
 @Getter @Setter
 public class UserVO {
-
+    
+ 
     private Long id;
+    @NotEmpty @NotBlank(message = "작성자 입력하세요")
     private String author;
+    @NotEmpty @NotBlank(message = "제목 입력하세요")
     private String title;
+    @NotEmpty @NotBlank(message = "내용 입력해주세요")
     private String content;
     private String regdate;
+    private int recnt;
 
 
     @Override
@@ -21,7 +32,8 @@ public class UserVO {
                 ", author='" + author + '\'' +
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
-                ", regDate='" + regdate + '\'' +
+                ", regdate='" + regdate + '\'' +
+                ", recnt=" + recnt +
                 '}';
     }
 }
