2dba8c5a8fd94b53b712ff825a9052123a944c42
woojin126 <woojin126@naver.com>
2021-05-12 23:51:34 +0900

    UserService

--- src/main/java/mybatis/board/service/UserService.java
+++ src/main/java/mybatis/board/service/UserService.java
@@ -1,5 +1,6 @@
 package mybatis.board.service;
 
+import mybatis.board.domain.Criteria;
 import mybatis.board.domain.UserVO;
 
 import java.util.List;
@@ -8,7 +9,9 @@ import java.util.Optional;
 public interface UserService {
 
 
-    public List<UserVO> getBoardList();
+    public List<UserVO> getBoardList(Criteria cri) throws Exception;
+    public int listCount() throws Exception;
     public void insertBoard(UserVO userVO);
     public UserVO findById(Long id);
+    public int updateViewCnt(Long id);
 }
