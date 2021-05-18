56b4f1e9334968ec648abfa6b6484c09f9f6024a
woojin126 <woojin126@naver.com>
2021-05-18 16:26:25 +0900

    삭제 수정 서비스추가

--- src/main/java/mybatis/board/service/UserService.java
+++ src/main/java/mybatis/board/service/UserService.java
@@ -1,17 +1,22 @@
 package mybatis.board.service;
 
+
 import mybatis.board.domain.Criteria;
 import mybatis.board.domain.UserVO;
+import org.apache.catalina.User;
+
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
 
 
     public List<UserVO> getBoardList(Criteria cri) throws Exception;
+
     public int listCount() throws Exception;
     public void insertBoard(UserVO userVO);
     public UserVO findById(Long id);
     public int updateViewCnt(Long id);
+    public void deleteById(Long id);
+    public void modifyBoard(UserVO userVO);
 }
