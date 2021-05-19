f2ad68e15f8606e5a44741658807249e79d8e47d
woojin126 <woojin126@naver.com>
2021-05-19 16:23:57 +0900

    수정 삭제 매퍼추가

--- src/main/java/mybatis/board/mapper/UserMapper.java
+++ src/main/java/mybatis/board/mapper/UserMapper.java
@@ -1,22 +1,24 @@
 package mybatis.board.mapper;
 
+
 import mybatis.board.domain.Criteria;
 import mybatis.board.domain.UserVO;
 import org.apache.ibatis.annotations.Mapper;
 
 
-
 import java.util.List;
-import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
 
 
     public List<UserVO> boardList(Criteria cri) throws Exception;
+
     public int listCount() throws Exception;
     public void insertBoard(UserVO userVO);
     public UserVO findByItem(Long id);
     public int updateViewCnt(Long id);
+    public void deleteById(long id);
+    public void modifyBoard(UserVO userVO);
 
 }
