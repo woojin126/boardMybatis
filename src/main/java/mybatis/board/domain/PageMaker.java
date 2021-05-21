8489f8699aba75024c4d38e6de06b424cdebc70c
woojin126 <woojin126@naver.com>
2021-05-21 20:01:11 +0900

    게시글 검색을위한 UriComponent 쿼리 추가

--- src/main/java/mybatis/board/domain/PageMaker.java
+++ src/main/java/mybatis/board/domain/PageMaker.java
@@ -3,8 +3,11 @@ package mybatis.board.domain;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
-public class PageMaker {
+
+public class PageMaker{
 
     private int totalCount;
     private int startPage;
@@ -53,6 +56,8 @@ public class PageMaker {
 
     private void calcData() {
         endPage = (int) (Math.ceil(cri.getPage() / (double)displayPageNum) * displayPageNum);
+        System.out.println("cri.getPage : " + cri.getPage());
+        System.out.println("displayPageNum : " + displayPageNum);
         System.out.println(endPage);
         startPage = (endPage - displayPageNum) + 1;
 
@@ -61,12 +66,16 @@ public class PageMaker {
             endPage = tempEndPage;
         }
         prev = startPage == 1 ? false : true;
-
+        System.out.println("endPage : " + endPage);
+        System.out.println("startPage : "  +startPage);
+        System.out.println("cri.getPerPageNum() : " + cri.getPerPageNum());
+        System.out.println("totalCount : " + totalCount);
         next = endPage * cri.getPerPageNum() >= totalCount ? false : true;
 
     }
 
-    public String makeQuery(int page) {
+    public String makeQuery(int page)
+    {
         UriComponents uriComponents =
                 UriComponentsBuilder.newInstance()
                         .queryParam("page", page)
@@ -76,6 +85,31 @@ public class PageMaker {
         return uriComponents.toUriString();
     }
 
+    public String makeSearch(int page){
+
+        UriComponents uriComponents =
+                UriComponentsBuilder.newInstance()
+                .queryParam("page",page)
+                .queryParam("perPageNum",cri.getPerPageNum())
+                .queryParam("searchType",((SearchCriteria)cri).getSearchType())
+                .queryParam("keyword", encoding(((SearchCriteria)cri).getKeyword()))
+                .build();
+        return uriComponents.toUriString();
+    }
+
+    /**
+     * 한글 인식 불가능으로 인코딩 필요
+     **/
+    private String encoding(String keyword){
+        if(keyword == null || keyword.trim().length() == 0)
+            return "";
+
+        try{
+            return URLEncoder.encode(keyword,"UTF-8");
+        }catch (UnsupportedEncodingException e){
+            return "";
+        }
+    }
 
 }
 
