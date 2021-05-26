4e166d0585deeba51ffa89bd9e86bc0736bcd926
woojin126 <woojin126@naver.com>
2021-05-26 14:48:57 +0900

    유효성 검증을위해 게시글 댓글 번호,내용부분이 검증 어노테이션 추가

--- src/main/java/mybatis/board/domain/reply/ReplyVO.java
+++ src/main/java/mybatis/board/domain/reply/ReplyVO.java
@@ -4,12 +4,16 @@ package mybatis.board.domain.reply;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter @Setter
 public class ReplyVO {
 
-    private int id;
-    private int rno;
+    private Long id;
+    private Long rno;
+    @NotBlank
     private String content;
+    @NotBlank
     private String author;
     private String regdate;
 
