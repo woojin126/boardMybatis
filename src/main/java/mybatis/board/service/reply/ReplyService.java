53066ed9e672375381fa431869507b188f5e04c2
woojin126 <woojin126@naver.com>
2021-05-27 12:35:03 +0900

    삭제,검증,컬럼조회,수정 서비스추가

--- src/main/java/mybatis/board/service/reply/ReplyService.java
+++ src/main/java/mybatis/board/service/reply/ReplyService.java
@@ -1,11 +1,17 @@
 package mybatis.board.service.reply;
 
 import mybatis.board.domain.reply.ReplyVO;
+import org.springframework.validation.Errors;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ReplyService {
 
     public List<ReplyVO> readReply(long id) throws Exception;
     public void writeReply(ReplyVO vo) throws Exception;
+    public void deleteReply(ReplyVO vo);
+    public Map<String, String> validateHandling(Errors errors);
+    public ReplyVO findById(long rno);
+    void modifyReply(ReplyVO vo);
 }
