26bf12ed897b5192a21fe5a8dc9a58968fc714e4
woojin126 <woojin126@naver.com>
2021-05-28 15:57:02 +0900

    댓글 수정, GET,POST 컨트롤러 기능 추가

--- src/main/java/mybatis/board/controller/ReplyController.java
+++ src/main/java/mybatis/board/controller/ReplyController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 import java.util.Map;
@@ -27,34 +25,57 @@ public class ReplyController {
     }
 
     @PostMapping("/deleteReply")
-    public String deleteReply(@PathVariable int rno, Model model){
+    public String deleteReply(@ModelAttribute ReplyVO vo,RedirectAttributes rttrs,@RequestParam long id){
+        log.debug("deleteReplyByReplyVO={}",vo);
+        replyService.deleteReply(vo);
 
-        replyService.deleteReply(rno);
+        rttrs.addAttribute("id",id);
 
-
-        return "redirect:/detailItem/{rno}";
+       return "redirect:/detailItem";
 
     }
 
     @PostMapping("/replyWrite")
-    public String replyWrite(@Valid ReplyVO vo, Errors errors, Model model, RedirectAttributes rttr)throws Exception{
-
-        if (valiationForm(vo, errors, rttr, "replyVO")) return "redirect:/detailItem/{id}";
+    public String replyWrite(@Valid ReplyVO vo, Errors errors, RedirectAttributes rttr,Model model)throws Exception{
+        log.debug("replyWriterContent={}",vo);
+        if (valiationForm(vo, errors,model, rttr, "replyVO")) return "redirect:/detailItem";
         replyService.writeReply(vo);
-        rttr.addAttribute("replyList",vo.getId());
-
-        return "redirect:/detailItem/{replyList}";
+        rttr.addAttribute("id",vo.getId());
+        return "redirect:/detailItem";
 
     }
 
-    private boolean valiationForm(@ModelAttribute @Valid ReplyVO vo, Errors errors, RedirectAttributes rttr, String updateLine) {
+    @GetMapping("/modifyReply")
+    public String modifyReplyGet(@RequestParam long rno, Model model){
+
+        ReplyVO replyColumn = replyService.findById(rno);
+
+        model.addAttribute("replyColumn",replyColumn);
+
+        return "board/modifyComment";
+    }
+
+    @PostMapping("/modifyReply")
+    public String modifyReplyPost(@ModelAttribute @Valid ReplyVO vo,Errors errors, RedirectAttributes rttr,Model model) throws Exception{
+
+        if (valiationForm(vo, errors,model, rttr, "replyColumn")) return "board/modifyComment";
+
+        replyService.modifyReply(vo);
+        rttr.addAttribute("id",vo.getId());
+        return "redirect:/detailItem";
+    }
+
+    private boolean valiationForm(@ModelAttribute @Valid ReplyVO vo, Errors errors,Model model ,RedirectAttributes rttr, String voName) {
         if (errors.hasErrors()) {
-           // rttr.addAttribute(updateLine, vo);
+
             Map<String, String> validatorResult = replyService.validateHandling(errors);
             for (String key : validatorResult.keySet()) {
+                log.debug("validationFormByErrorKey={}",key);
                 rttr.addAttribute(key, validatorResult.get(key));
+                model.addAttribute(key,validatorResult.get(key));
             }
-            rttr.addAttribute("id",vo.getId());
+            rttr.addAttribute("id",vo.getId()); //error 내용을들 가지고 다시 detailItem 페이지를 열기위해 같이 id값 보냄
+            model.addAttribute(voName, vo);
             return true;
         }
         return false;
