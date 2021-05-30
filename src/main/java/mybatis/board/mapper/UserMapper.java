144e9234f52eab09fa7c48f0d468449cc645ff06
woojin126 <woojin126@naver.com>
2021-05-30 16:53:51 +0900

    userMapper insertFile selectFileList selectFileInfo 추가 작성할때 업로드한 file 보이도록하기

--- src/main/java/mybatis/board/mapper/UserMapper.java
+++ src/main/java/mybatis/board/mapper/UserMapper.java
@@ -8,16 +8,20 @@ import org.apache.ibatis.annotations.Mapper;
 
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface UserMapper {
 
 
-    public List<UserVO> boardList(SearchCriteria scri) throws Exception;
-    public int listCount(SearchCriteria scri) throws Exception;
-    public int insertBoard(UserVO userVO);
-    public UserVO findByItem(Long id);
-    public int updateViewCnt(Long id);
-    public void deleteById(long id);
-    public void modifyBoard(UserVO userVO);
+    List<UserVO> boardList(SearchCriteria scri) throws Exception;
+    int listCount(SearchCriteria scri) throws Exception;
+    void insertBoard(UserVO userVO);
+    UserVO findByItem(Long id);
+    int updateViewCnt(Long id);
+    void deleteById(long id);
+    void modifyBoard(UserVO userVO);
+    void insertFile(Map<String, Object> map) throws Exception;
+    List<Map<String,Object>> selectFileList(long id) throws Exception;
+    Map<String,Object> selectFileInfo(Map<String,Object> map)throws Exception;
 }
