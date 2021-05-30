95deb1733dd5662faabe99795b70cc967acc5bcd
woojin126 <woojin126@naver.com>
2021-05-30 16:57:13 +0900

    userMapper insertFile userService selectFileList (선택한 id값을 이용하여 컬럼리스트가져오기) selectFileInfo 작성할때 업로드한 file 보이도록하기

--- src/main/java/mybatis/board/service/user/UserService.java
+++ src/main/java/mybatis/board/service/user/UserService.java
@@ -6,6 +6,7 @@ import mybatis.board.domain.user.SearchCriteria;
 import mybatis.board.domain.user.UserVO;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 
 import java.util.HashMap;
@@ -17,15 +18,16 @@ public interface UserService {
 
     public List<UserVO> getBoardList(SearchCriteria scri) throws Exception;
     public int listCount(SearchCriteria scri) throws Exception;
-    public int insertBoard(UserVO userVO);
+    public void insertBoard(UserVO userVO, MultipartHttpServletRequest mpRequest) throws Exception;
     public UserVO findById(Long id);
     public int updateViewCnt(Long id);
     public void deleteById(Long id);
     public void modifyBoard(UserVO userVO);
+    public List<Map<String,Object>> selectFileList(long id)throws Exception;
+    Map<String,Object> selectFileInfo(Map<String,Object> map)throws Exception;
     /**
      *검증코드 제목, 작성자 ,내용 중  NULL 값이나 , "" 빈문자열 올시 error 메세지 반환
      */
     public Map<String, String> validateHandling(Errors errors);
-
 
 }
