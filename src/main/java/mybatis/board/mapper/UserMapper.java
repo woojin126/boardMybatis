48fe3bd3d97554fa1f950eeca549054a369b8a52
woojin126 <woojin126@naver.com>
2021-06-06 21:04:21 +0900

    시큐리티 유저 매퍼

--- src/main/java/mybatis/board/mapper/UserMapper.java
+++ src/main/java/mybatis/board/mapper/UserMapper.java
@@ -1,28 +1,26 @@
 package mybatis.board.mapper;
 
-
-import mybatis.board.domain.reply.ReplyVO;
-import mybatis.board.domain.user.SearchCriteria;
+import mybatis.board.domain.board.BoardVO;
 import mybatis.board.domain.user.UserVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
 
 @Mapper
 public interface UserMapper {
+    //유저 정보
+    ArrayList<UserVO> findByUserId(@Param("userId") String userId);
 
+    //유저 저장
+    int userSave(UserVO boardVO);
 
-    List<UserVO> boardList(SearchCriteria scri) throws Exception;
-    int listCount(SearchCriteria scri) throws Exception;
-    void insertBoard(UserVO userVO);
-    UserVO findByItem(Long id);
-    int updateViewCnt(Long id);
-    void deleteById(long id);
-    void modifyBoard(UserVO userVO);
-    void insertFile(Map<String, Object> map) throws Exception;
-    List<Map<String,Object>> selectFileList(long id) throws Exception;
-    Map<String,Object> selectFileInfo(Map<String,Object> map)throws Exception;
-    void updateFile(Map<String,Object> map) throws Exception;
+    //유저 권한 저장
+    int userRoleSave(@Param("userNo") Long userNo,@Param("roleNo") Long roleNo);
+
+    //유저 FK번호 알아내기
+    int findUserNo(@Param("userId") String userId);
+
+    //권한 FK번호 알아내기
+    int findRoleNo(@Param("roleName") String roleName);
 }
