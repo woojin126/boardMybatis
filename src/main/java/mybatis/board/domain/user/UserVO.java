4d7823d048e5a201d5d5fc3fed6d5371c6eefa34
woojin126 <woojin126@naver.com>
2021-06-06 21:06:16 +0900

    시큐리티 vo

--- src/main/java/mybatis/board/domain/user/UserVO.java
+++ src/main/java/mybatis/board/domain/user/UserVO.java
@@ -1,45 +1,18 @@
 package mybatis.board.domain.user;
+
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-import javax.validation.constraints.NotBlank;
-
-
-
-/**
- * @valid 유효성 검증을위해 @NotEmpty @NotBlank 설정 
- * 공백문자나 , NULL 오면 Errors에 들어감
- */
-
-@Setter @Getter
+@Getter
+@Setter
+@ToString
 public class UserVO {
 
+    private Long userNo;
+    private String userId;
+    private String password;
+    private String name;
 
-    private Long id;
-    @NotBlank(message = "작성자 입력하세요")
-    private String author;
-    @NotBlank(message = "제목 입력하세요")
-    private String title;
-    @NotBlank(message = "내용 입력해주세요")
-    private String content;
-    private String regdate;
-    private int recnt;
-
-
-
-    @Override
-    public String toString() {
-        return "UserVO{" +
-                "id=" + id +
-                ", author='" + author + '\'' +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", regdate='" + regdate + '\'' +
-                ", recnt=" + recnt +
-                '}';
-    }
-
-
-
+    private String roleName;
 }
-
